960f64a482dd7e192ebe96159af773c4d18c3579
Jonas Matser <jonas@jonasmatser.nl>
2015-04-25 22:21:09 +0200

    Implement Worker/Stealer deque using crate based on original green-threads Switch to ScopedPool since respawning threads on panic doesn't seem useful since we're only submitting a Scheduler job for each thread once

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 extern crate threadpool;
+extern crate deque;
 
 use std::sync::mpsc::{ channel };
 use std::sync::mpsc::{ Sender, Receiver };
 
-use threadpool::{ ScopedPool, ThreadPool };
+use threadpool::{ ScopedPool };
+use deque::BufferPool;
 
-type thread_num = usize;
-type pool_type = ThreadPool;
-
-const NUM_THREADS: thread_num = 2;
+const NUM_THREADS: u32 = 2;
 
 #[derive(Debug)]
 enum SchedulerMessage {
@@ -16,29 +15,70 @@ enum SchedulerMessage {
 }
 
 struct Scheduler {
-    num: thread_num,
+    num: u32,
     sender: Sender<SchedulerMessage>,
+    worker: deque::Worker<Task>,
+    task_receiver: Receiver<Task>,
 }
 
 impl Scheduler {
-    fn run(&self) {
-        for _ in 0..10 {
-            print!("{}", self.num);
-        };
-        println!("");
+    fn run(&mut self) {
+
+        loop {
+            while let Some(task) = self.worker.pop() {
+                println!("{:?}", task);
+            };
+
+            match self.task_receiver.recv() {
+                Ok(task) => {
+                    println!("{:?}", task);
+                }
+                Err(err) => {
+                    break;
+                }
+            }
+        }
+
         self.sender.send(SchedulerMessage::Done).ok();
     }
 }
 
-fn main() {
-    let threadpool = pool_type::new(NUM_THREADS);
+#[derive(Debug)]
+struct Task {
+    id: u32,
+}
 
-    let (sender, receiver) = channel();
+fn main() {
+    // debugger task_id
+    let mut task_id: u32 = 0;
+
+    // create pool with 1 thread per core
+    let threadpool = ScopedPool::new(NUM_THREADS);
+
+    // shared bufferpool for worker/stealer deques
+    let deque_pool: deque::BufferPool<Task> = deque::BufferPool::new();
+
+    // store stealers for cloning for each scheduler
+    let mut stealers = Vec::new();
+
+    let mut task_senders = Vec::new();
+
+    let (scheduler_sender, scheduler_receiver) = channel();
 
     for num in 0..NUM_THREADS {
-        let scheduler = Scheduler {
+        let (mut worker, mut stealer) = deque_pool.deque();
+        stealers.push(stealer);
+
+        let (task_sender, task_receiver) = channel();
+        task_sender.send(Task { id: task_id });
+        task_id += 1;
+        task_senders.push(task_sender);
+
+        let mut scheduler = Scheduler {
             num: num,
-            sender: sender.clone(),
+            sender: scheduler_sender.clone(),
+            worker: worker,
+            task_receiver: task_receiver,
         };
 
         threadpool.execute(move|| {
@@ -49,9 +89,15 @@ fn main() {
     // drop sender, so all senders are owned by the
     // threadpool and receiver will be done when
     // threadpool is done
-    drop(sender);
+    drop(scheduler_sender);
 
-    receiver.iter().map(|msg| println!("{:?}", msg)).collect::<Vec<_>>();
+    // for debugging purposes drop task_sender so
+    // that threads end
+    drop(task_senders);
+
+    while let Ok(msg) = scheduler_receiver.recv() {
+        println!("{:?}", msg);
+    }
 }
 
 #[test]
