02de62490e4fbe5f0c17676fe2d7b6b1987d38d0
Jonas Matser <jonas@jonasmatser.nl>
2015-04-25 22:39:03 +0200

    Make single mpmc task_sender for all threads

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate deque;
 
 use std::sync::mpsc::{ channel };
 use std::sync::mpsc::{ Sender, Receiver };
+use std::sync::{ Arc, Mutex };
 
 use threadpool::{ ScopedPool };
 use deque::BufferPool;
@@ -18,7 +19,7 @@ struct Scheduler {
     num: u32,
     sender: Sender<SchedulerMessage>,
     worker: deque::Worker<Task>,
-    task_receiver: Receiver<Task>,
+    task_receiver: Arc<Mutex<Receiver<Task>>>,
 }
 
 impl Scheduler {
@@ -26,12 +27,18 @@ impl Scheduler {
 
         loop {
             while let Some(task) = self.worker.pop() {
-                println!("{:?}", task);
+                println!("thread {}: {:?}", self.num, task);
             };
 
-            match self.task_receiver.recv() {
+            // only lock long enough to receive a job
+            let msg = {
+                let lock = self.task_receiver.lock().unwrap();
+                lock.recv()
+            };
+
+            match msg {
                 Ok(task) => {
-                    println!("{:?}", task);
+                    println!("thread {}: {:?}", self.num, task);
                 }
                 Err(err) => {
                     break;
@@ -61,24 +68,26 @@ fn main() {
     // store stealers for cloning for each scheduler
     let mut stealers = Vec::new();
 
-    let mut task_senders = Vec::new();
+    // channel for mpmc queue
+    let (task_sender, task_receiver) = channel();
+    // make Send + Sync receiver
+    let task_receiver = Arc::new(Mutex::new(task_receiver));
 
+    // channel for receiving info from schedulers
     let (scheduler_sender, scheduler_receiver) = channel();
 
     for num in 0..NUM_THREADS {
         let (mut worker, mut stealer) = deque_pool.deque();
         stealers.push(stealer);
 
-        let (task_sender, task_receiver) = channel();
         task_sender.send(Task { id: task_id });
         task_id += 1;
-        task_senders.push(task_sender);
 
         let mut scheduler = Scheduler {
             num: num,
             sender: scheduler_sender.clone(),
             worker: worker,
-            task_receiver: task_receiver,
+            task_receiver: task_receiver.clone(),
         };
 
         threadpool.execute(move|| {
@@ -93,7 +102,7 @@ fn main() {
 
     // for debugging purposes drop task_sender so
     // that threads end
-    drop(task_senders);
+    drop(task_sender);
 
     while let Ok(msg) = scheduler_receiver.recv() {
         println!("{:?}", msg);
